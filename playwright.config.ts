47c861e96da4977a188c877ad0f3d08b7a63ea33
michal.bandyszak <michal.bandyszak@globallogic.com>
2024-05-07 14:04:54 +0200

    Changed tracing to save on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     video: 'retain-on-failure'
+    // trace: 'on',
+    // video: 'on'
   },
 
   /* Configure projects for major browsers */
