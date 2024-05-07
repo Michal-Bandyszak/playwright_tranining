7d8a6068930d9e9bc752179077c0a0349dfc142f
michal.bandyszak <michal.bandyszak@globallogic.com>
2024-05-07 14:06:03 +0200

    Added new method and login data

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -5,4 +5,14 @@ export class LoginPage {
     loginInput = this.page.getByTestId('login-input')
     passwordInput = this.page.getByTestId('password-input')
     loginButton = this.page.getByTestId('login-button')
+
+    loginError_id = this.page.getByTestId('error-login-id')
+    loginError_password = this.page.getByTestId('error-login-password')
+
+    async login(userId: string, userPassword: string): Promise<void> {
+        await this.loginInput.fill(userId);
+        await this.passwordInput.fill(userPassword);
+        await this.loginButton.click();
+    }
+
 }
